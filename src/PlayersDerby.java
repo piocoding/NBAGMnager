050cc9869b6dc65e28205ce6751184e67e2cc904
nuraisya <nuraisyaczy@gmail.com>
2024-05-24 20:01:39 +0800

    change db location

--- src/PlayersDerby.java
+++ src/PlayersDerby.java
@@ -15,7 +15,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class PlayersDerby {
-    private static final String playerdb = "jdbc:derby:MyDatabase"; // playerdb URL
+    private static final String playerdb = "jdbc:derby:statsdb;create=true"; // playerdb URL
 
     public static void main(String[] args) {
         try (Connection connection = DriverManager.getConnection(playerdb)) {
