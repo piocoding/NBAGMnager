7f06382d667dbb3cee4677586be3184eae746ffd
Ayu Sabrina <Ayu Sabrina@SABRINA>
2024-06-08 14:37:10 +0800

    Merge origin/master

--- src/Journey.java
+++ src/Journey.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
  * @author Ayu Sabrina
  */
 public class Journey {
-    private final int X = Integer.MAX_VALUE;
+    private final int X = Integer.MAX_VALUE; // no edge exists
     private final int[][] distance = {
         {0   ,X   ,X   ,X   ,X   ,500 ,1137,X   ,678 ,983 },
         {X   ,0   ,X   ,X   ,554 ,X   ,X   ,1507,2214,X   },
@@ -24,16 +24,16 @@ public class Journey {
         {678 ,2214,X   ,X   ,1901,X   ,X   ,942 ,0   ,778 },
         {983 ,X   ,2584,X   ,X   ,X   ,458 ,X   ,778 ,0   }       
     };
-    private final String[] cities;
-    private ArrayList<Integer> pathIndex = new ArrayList<>();
-    private boolean[] visited = new boolean[distance.length];
-    private int totalDistance;
-    private int current;   
+    private final String[] cities; // name of each cities
+    private ArrayList<Integer> path = new ArrayList<>(); // store travel path
+    private boolean[] visited = new boolean[distance.length]; // check if the city is visited
+    private int totalDistance; 
+    private int current; 
 
     public Journey() {    
         cities = new String[]{"Spurs", "Warriors", "Celtics", "Heat", "Lakers", "Suns", "Magic", "Nuggets", "Thunder", "Rockets"};
         totalDistance = 0;
-        current = 0;  
+        current = 0; // started at Spurs 
         findShortestPath();
     }
     
@@ -44,23 +44,31 @@ public class Journey {
     }
         
     public void findShortestPath(){
-        visited[current] = true;
-        pathIndex.add(current);
+        visited[current] = true; // mark the current node as visited
+        path.add(current); // add current node to the path
         
+        // iterate through the nodes
         for (int i=0;i<distance.length-1;i++){
+            // find the next destination
             int next=findNearestNeighbour();
+            
             totalDistance+=distance[current][next];
+            
             visited[next]= true;
-            pathIndex.add(next);
+            path.add(next);
             current=next;
         }
     }
-  
+    
+    // find the nearest unvisited neighbor
     private int findNearestNeighbour(){
         int nearest = -1;
         int minDistance = Integer.MAX_VALUE;
         
+        // iterate through all nodes
         for (int i = 0; i < distance.length; i++) {
+            
+            // check if the node is unvisited and if the distance is less than the current minimum
             if (!visited[i] && distance[current][i] < minDistance) {
                 nearest = i;
                 minDistance = distance[current][i];
@@ -69,24 +77,28 @@ public class Journey {
         return nearest;
     }
     
+    // print the travel plan in a list
     public String getSchedule(){
         StringBuilder pathString = new StringBuilder();
-        for (int i=0;i<pathIndex.size();i++)
-            pathString.append((i+1)).append(". ").append(cities[pathIndex.get(i)]).append("\n");  
+        for (int i=0;i<path.size();i++)
+            pathString.append((i+1)).append(". ").append(cities[path.get(i)]).append("\n");  
         return pathString.toString();
     }
     
+    // total distance travelled in KM
     public String getTotalDistance(){
         StringBuilder distanceString = new StringBuilder();
         distanceString.append("\nTotal distance traveled: ").append(totalDistance).append(" KM");
         return distanceString.toString();
     }
     
+    // justifications of the travel plan
     public String getReasoning(){
-        return "This travel plan is the best because:\n"
-                + "1. minimized physical and mental fatigue due to traveling\n" 
-                + "2. reduced travel stress from continuous long-distance travel\n"
-                + "3. less travel times, more preparation time";
+        return """
+               This travel plan is the best because:
+               1. minimized physical and mental fatigue due to traveling
+               2. reduced travel stress from continuous long-distance travel
+               3. less travel times, more preparation time""";
     } 
 }
 
--- src/PlayerStats.java
+++ src/PlayerStats.java
@@ -20,7 +20,7 @@ import java.sql.ResultSet;
  */
 
 public class PlayerStats {
-    private static final String csvFile = "statistics.csv"; //change accordingly
+    private static final String csvFile = "statistics.csv"; 
     private static final String statsUrl = "jdbc:derby://localhost:1527/NBAdb;create=true";
     private static Connection connection = null;
     
@@ -30,7 +30,9 @@ public class PlayerStats {
     
     public static void statsdatabase(){
         try{
+            // load the Derby JDBC driver
             Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+            // establish the connection to the databse
             connection = DriverManager.getConnection(statsUrl);
             System.out.println("Database is connected.");
             
@@ -45,6 +47,7 @@ public class PlayerStats {
     
     public static void createTable() throws SQLException{
         try{
+            // SQL statement to create the statistics table
             String createTable = "CREATE TABLE statistics (" +
                 "name VARCHAR(50)," +
                 "position VARCHAR(5)," +
@@ -55,14 +58,16 @@ public class PlayerStats {
                 "blocks DOUBLE," +
                 "points DOUBLE" +
                 ")";
-
-        Statement stmt = connection.createStatement();
         
-        //execute SQL statement to create the table
-        stmt.executeUpdate(createTable);
+            // create a statement object to execute the SQL statement
+            Statement stmt = connection.createStatement();
+
+            // execute SQL statement to create the table
+            stmt.executeUpdate(createTable);
             System.out.println("Table successfully created.");
         
         }catch(SQLException e){
+            // handle the case where the table already created
             if (!"X0Y32".equals(e.getSQLState())) { // Table already exists
                 e.printStackTrace();
             }
@@ -71,6 +76,7 @@ public class PlayerStats {
     
     public static void csvToDatabase(){
         try{
+            // SQL statement to count the rows in the statistics table
             String sqlCount = "SELECT COUNT(*) FROM APP.STATISTICS";
             PreparedStatement countStmt = connection.prepareStatement(sqlCount);
             ResultSet rs = countStmt.executeQuery();
@@ -78,31 +84,34 @@ public class PlayerStats {
             if(rs.next())
                 rowCount = rs.getInt(1);
             
+            // exit the method if data already added
             if(rowCount == 572)
                 return;
             
             BufferedReader reader = new BufferedReader(new FileReader(csvFile));
             
             String line;
-            //skip the header line if present
+            // skip the header line if present
             reader.readLine();
             
-            //prepare SQL INSERT statement
+            // prepare SQL INSERT statement
             String sql = "INSERT INTO statistics (name, position, age, rebounds, assists, steals, blocks, points) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
             PreparedStatement pstmt = connection.prepareStatement(sql);
 
             // Read and insert data from CSV
             while ((line = reader.readLine()) != null) {
-                String[] data = line.split(","); // Assuming CSV delimiter is comma
+                String[] data = line.split(","); // split the line into fields using comma
+                
                 // Set values for each column in the prepared statement
                 pstmt.setString(1, data[0]); // PLAYER
-                pstmt.setString(2, data[1]); // POS
+                pstmt.setString(2, data[1]); // POS (position)
                 pstmt.setInt(3, Integer.parseInt(data[2])); // AGE
-                pstmt.setDouble(4, Double.parseDouble(data[3])); // TRB
-                pstmt.setDouble(5, Double.parseDouble(data[4])); // AST
-                pstmt.setDouble(6, Double.parseDouble(data[5])); // STL
-                pstmt.setDouble(7, Double.parseDouble(data[6])); // BLK
-                pstmt.setDouble(8, Double.parseDouble(data[7])); // PTS
+                pstmt.setDouble(4, Double.parseDouble(data[3])); // TRB (rebounds)
+                pstmt.setDouble(5, Double.parseDouble(data[4])); // AST (assists)
+                pstmt.setDouble(6, Double.parseDouble(data[5])); // STL (steals)
+                pstmt.setDouble(7, Double.parseDouble(data[6])); // BLK (blocks)
+                pstmt.setDouble(8, Double.parseDouble(data[7])); // PTS (points)
+                
                 // Execute the INSERT statement
                 pstmt.executeUpdate();
             }
@@ -112,6 +121,4 @@ public class PlayerStats {
             e.printStackTrace();
         }
     }
-    
-    
 }
